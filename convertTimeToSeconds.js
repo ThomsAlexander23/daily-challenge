295ff966e8fec2b02cfa81c019138fd6ef7f86c5
Alex <thoms.alexander23@gmail.com>
2021-03-02 21:20:04 -0500

    Feat: refactored to increase flexibilty to seconds and for the type check to take in unlimited arguments

--- convertTimeToSeconds.js
+++ convertTimeToSeconds.js
@@ -5,8 +5,12 @@
 */
 
 // function to ensure type of input is an interger
-function typeInteger(input){
-  return !isNaN(parseInt(input))
+function typeInteger(...inputs){
+  let array = []
+  for (let i = 0; i< inputs.length; i++){
+     array.push(!isNaN(parseInt(inputs[i])))
+  }
+  return array.every(Boolean)
 }
 
 // function to convert minutes to seconds
@@ -19,12 +23,22 @@ function hrToSec(input){
   return (input * 60)* 60
 }
 
+// function to convert hours and mins to seconds
+function timeToSeconds(hours, minutes, seconds){
+  seconds += (hours * Math.pow(60,2) + (minutes * 60))
+  return seconds
+}
+console.log(timeToSeconds(1,3))
+console.log(timeToSeconds(2,0))
+console.log(timeToSeconds(2,0,20))
+
 // function to convert the time to seconds only
-function convertToSeconds(hours, minutes){
-return (typeInteger(hours) && typeInteger(minutes))
-  ? (minToSec(minutes) + (hrToSec(hours)))
+function convertToSeconds(hours, minutes, seconds){
+return (typeInteger(hours, minutes, seconds))
+  ? timeToSeconds(hours,minutes,seconds)
   : "Incorrect input"
 }
 
 console.log(convertToSeconds(1,3))
 console.log(convertToSeconds(2,0))
+console.log(convertToSeconds(2,0,20))
