73293641bdd274bf30888be9089300f931177651
Alex <thoms.alexander23@gmail.com>
2021-03-04 18:07:09 -0500

    Feat: completed the exercise

--- additiveNumber.js
+++ additiveNumber.js
@@ -19,14 +19,52 @@
 // 3 <= num.length <= 35
 
 
-// functions to check correct input type
-// function for type string containing only numbers
-function numString(string){
-  if ((string === "string") && (parseFloat(string) == string) && (3 <= string.length <= 35)){
-    return true
-  }
-  return false
-}
+function isAdditiveNumber(numStr){
+   if(numStr.length < 3) {return false;}
+   let str = "";
+   let check = true;
+   for(let i = numStr.length - 1; i > 1; i--){
+      str = numStr[i] + str;
+      if(numStr[i] === "0") {continue;}
+      let num1 = str;
+      let num2 = numStr[i - 1]
+      for(let j = i - 2; j >= 0; j--){
+         if((num2[0] === "0") && num2.length > 1){
+            num2 = numStr[j] + num2
+            check = false;
+         } 
+         else if(parseInt(num1) >= parseInt(num2)){
+            let diff = num1 - num2;
+            if(numStr.slice(0, j + 1).endsWith(diff)){
+               num1 = num2;
+               num2 = diff;
+               let ind = Math.floor(Math.log10(diff));
+               console.log(ind)
+               ind = ind < 0 ? 0 : ind
+               j -= ind;
+               console.log(j)
+               check = true;
+            }
+            else {
+               num2 = numStr[j] + num2
+               check = false;
+            }
+         }
+         else{
+            check = false;
+            break;
+         }
+      }
+      if(check) return check;
+   };
+   return check;
+};
 
-
-
+let stringOne = "11235813213455"
+let stringTwo = "12334578123201"
+let stringThree = "199100199"
+let stringFour = "112345"
+console.log(isAdditiveNumber(stringOne));
+console.log(isAdditiveNumber(stringTwo));
+console.log(isAdditiveNumber(stringThree));
+console.log(isAdditiveNumber(stringFour));
