a921e270eb8394573b4a3bdf3c62e7d5fdf5c26e
Alex <thoms.alexander23@gmail.com>
2021-03-07 12:55:33 -0500

    Initial Commit

---
+++ positivePrimeCheck.js
@@ -0,0 +1,23 @@
+/*
+  Create a function that returns a boolean of true if a number is prime positive, and false otherwise. A prime number is any positive integer that is evenly divisible by only two divisors one and itself.
+
+  first ten prime number are: 2,3,5,7,11,13,17,19,23,29
+
+  examples:
+
+    isPrime(31) === true
+    isPrime(18) === false
+    isPrime(11) === true
+
+  constraints:
+
+  A prime number has no other factors except 1 and itself.
+
+  If a number is odd it is not divisible by an even number.
+
+  1 is not considered a prime number.
+
+  Author: Alex
+  Version: 03072021
+*/
+
